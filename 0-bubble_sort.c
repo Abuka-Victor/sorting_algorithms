298b05668a3a1103c11893c362d0f8c1fe01c5eb
Abuka-Victor <abukavictoro@gmail.com>
2022-07-19 14:30:01 +0100

    file setup

---
+++ 0-bubble_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Implements the bubble sort algorithm
+ * @array: The array pointer
+ * @size: The size of the array
+ *
+ * Return: Null Void
+ */
+void bubble_sort(int *array, size_t size)
+{
+}
